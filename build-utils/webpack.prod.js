861c4b7b0f559cb7256b44ae1c98c651264c5965
kanatamon <ponde.moonlord@gmail.com>
2017-11-18 14:01:50 +0700

    Config css loader for production

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,5 +1,21 @@
-const config = {
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
+const config = {
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ExtractTextWebpackPlugin.extract({
+          use: 'css-loader',
+          fallback: 'style-loader',
+        }),
+      }
+    ],
+  },
+  plugins: [
+    new ExtractTextWebpackPlugin('style.css'),
+  ],
 };
 
 module.exports = config;
