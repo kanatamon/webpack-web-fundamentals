08912c94cdb0fe6d46cafafd16599c97ef48bcac
kanatamon <ponde.moonlord@gmail.com>
2017-11-18 14:36:03 +0700

    Config compression plugin

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,5 +1,6 @@
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
 const config = {
   devtool: 'source-map',
@@ -18,6 +19,12 @@ const config = {
     new ExtractTextWebpackPlugin('style.css'),
     new UglifyJsWebpackPlugin({
       sourceMap: true,
+    }),
+    new CompressionWebpackPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|html|css)$/,
+      minRatio: 0.8,
     }),
   ],
 };
